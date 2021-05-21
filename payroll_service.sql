25b48cbc807ec2b72ec9fec1daba9b780a5cce1c
victor-singha <vicunite2@gmail.com>
2021-05-21 12:43:11 +0530

    insert data

--- payroll_service.sql
+++ payroll_service.sql
@@ -6,11 +6,16 @@
 
 --select db_name(); 
 
-create table employee_payroll
-(
-id int identity(1,1) primary key,
-name varchar(100) not null,
-salary money not null,
-start date not null
-);
+--create table employee_payroll
+--(
+--id int identity(1,1) primary key,
+--name varchar(100) not null,
+--salary money not null,
+--start date not null
+--);
+
+insert into employee_payroll values
+('Billi',100000.0,'2018-03-21'),
+('Terisa',200000.0,'2019-04-16'),
+('Johnathan',300000.0,'2021-01-20');
 
