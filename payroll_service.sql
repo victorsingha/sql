cfb71c2c5bef757fb341d608f59c2545342a3ce7
victor-singha <vicunite2@gmail.com>
2021-05-21 12:32:12 +0530

    create table

--- payroll_service.sql
+++ payroll_service.sql
@@ -1,5 +1,16 @@
-create database payroll_service
+--create database payroll_service
 
-select * from sys.databases;
+--select * from sys.databases;
 
-use payroll_service
+--use payroll_service;
+
+--select db_name(); 
+
+create table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(100) not null,
+salary money not null,
+start date not null
+);
+
